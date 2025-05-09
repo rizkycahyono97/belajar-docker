ff744d351be2ec654164737d332e8234a2d32d10
rizkycahyono97 <rizkycahyonoputra2004@gmail.com>
2025-05-09 19:53:28 +0700

    from, label

--- Dockerfile/script.sh
+++ Dockerfile/script.sh
@@ -6,3 +6,18 @@ docker build -t rizkycahyono/run run
 
 docker build -t rizkycahyono/run run --progress=plain --no-cache # lebih detail untuk informasi
 
+# COMMAND Instruction
+docker build -t rizkycahyono/command command
+
+docker image inspect rizkycahyono/command command
+
+docker container create --name command rizkycahyono/command
+
+docker container start command
+
+docker container logs command
+
+# LABEL Instruction
+docker build -t rizkycahyono/label label
+
+docker image inspect rizkycahyono/label label
