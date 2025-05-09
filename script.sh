f62a40148aa1393525c53f1f5cc83e4fa244ff58
rizkycahyono97 <rizkycahyonoputra2004@gmail.com>
2025-05-09 16:41:42 +0700

    first commit

---
+++ script.sh
@@ -0,0 +1,8 @@
+# FROM Instruction
+docker build -t rizkycahyono/from from 
+
+# RUN Instruction
+docker build -t rizkycahyono/run run
+
+docker build -t rizkycahyono/run run --progress=plain --no-cache # lebih detail untuk informasi
+
